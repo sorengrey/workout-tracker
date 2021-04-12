c270fe7cc83a13571b5b61345b072a337c1319d8
Heather Smith <halexsmith86@gmail.com>
2021-04-12 14:38:50 -0400

    Added workout model

---
+++ Develop/models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for this workout"
+  },
+  type: {
+    type: Number,
+    required: "What kind of workout was it?"
+  },
+  weight: {
+    type: Number,
+    required: "How much weight did you use?",
+    default: 0
+  },
+  sets: {
+    type: Number,
+    required: "How many sets did you do?",
+    default: 0
+  },
+  reps: {
+    type: Number,
+    required: "How many reps did you do?",
+    default: 0
+  },
+  duration: {
+    type: String,
+    trim: true,
+    required: "How long was your workout?"
+  },
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
