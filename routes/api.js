451cce6246f68c69be5fa62dc22df2c3102a24f2
Heather Smith <halexsmith86@gmail.com>
2021-04-14 17:26:12 -0400

    Added routes: /exercise and /stats

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+const path = require("path");
 
-// this is supposed to get the /workout page - not working yet
-router.get("/api/workout", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "./../public/exercise.html"))
+})
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "./../public/stats.html"))
+})
 
 // this is supposed to post the workout info
 router.post("/api/workout", ({ body }, res) => {
