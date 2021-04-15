44624bd72118eac95d66f53d9dbd1523f2253dde
Heather Smith <halexsmith86@gmail.com>
2021-04-15 12:48:53 -0400

    Added api routes for stats and exercise

--- public/db.js
+++ public/db.js
@@ -1,25 +1,80 @@
 let db;
 let workoutVersion;
 
-// creates a new db request for a "workoutsDB" database
-const request = indexedDB.open('workoutsDB', workoutVersion || 21);
+// creates a new db request for a "workoutDB" database
+const request = indexedDB.open("workoutDB", workoutVersion || 21);
 
 request.onupgradeneeded = function (e) {
-    console.log('Upgrade needed in IndexDB');
-  
-    const { oldVersion } = e;
-    const newVersion = e.newVersion || db.version;
-  
-    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-  
-    db = e.target.result;
-  
-    if (db.objectStoreNames.length === 0) {
-      db.createObjectStore('WorkoutStore', { autoIncrement: true });
+  console.log("Upgrade needed in IndexDB");
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("workoutDB", { autoIncrement: true });
+  }
+};
+
+function checkDatabase() {
+  console.log("check db invoked");
+
+  // Open a transaction on your workout db
+  let transaction = db.transaction(["workoutDB"], "readwrite");
+
+  // access your workoutDB object
+  const store = transaction.objectStore("workoutDB");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are stored workouts, we need to bulk add them when we are back online (you might need to change /stats/ to /exercise/)
+    if (getAll.result.length > 0) {
+      fetch("/api/stats/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If our returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(["workoutDB"], "readwrite");
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore("workoutDB");
+
+            // Clear existing entries because our bulk add was successful
+            currentStore.clear();
+            console.log("Clearing store 🧹");
+          }
+        });
     }
   };
+}
 
 // error handling
 request.onerror = function (e) {
-    console.log(`Woops! ${e.target.errorCode}`);
-  };
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+// success handling if everything is ok with the db
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+
+  // Check if app is online before reading from the db
+  if (navigator.onLine) {
+    console.log("Backend online! 🗄️");
+    checkDatabase();
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,39 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const path = require("path");
 
+
+// gets the /exercise page
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname + "./../public/exercise.html"))
 })
 
+// gets the /stats page
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "./../public/stats.html"))
 })
 
-// this is supposed to post the workout info
-router.post("/api/workout", ({ body }, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+// gets the api/stats page
+router.get("/api/stats", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// gets the api/exercise page
+router.get("/api/exercise", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
