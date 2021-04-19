dc7c50435e944bca64fba2bffcd16c5db4491364
Heather Smith <halexsmith86@gmail.com>
2021-04-19 13:37:36 -0400

    deploying

--- public/exercise.js
+++ public/exercise.js
@@ -1,4 +1,3 @@
-// const Workout = require("./models/workout.js")
 const workoutTypeSelect = document.querySelector("#type");
 const cardioForm = document.querySelector(".cardio-form");
 const resistanceForm = document.querySelector(".resistance-form");
